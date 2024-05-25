c05a8b73d1c1bcc03bec1d63e596d519b77ec0a3
ritchiejames <automatealunkal@gmail.com>
2024-05-25 19:03:17 +0400

    added private ip but there is a problem with two private ip.need to check on detachment of interface

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 
-provider "aws" {
-  region = "us-east-1"
-}
+#provider "aws" {
+ # region = "us-east-1"
+#}
 
 ##Jenkins server 
 resource "aws_instance" "Jenkins_server" {
@@ -37,11 +37,26 @@ resource "aws_instance" "Docker_host" {
   ami           = "ami-0bb84b8ffd87024d8"
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.Jenkins_SG.id]
-
-  tags = {
+    tags = {
     Name = "Docker_host"
   }
 }
+#Private ip interface
+
+resource "aws_network_interface_attachment" "private_attachment" {
+    instance_id = aws_instance.Docker_host.id
+    device_index = 1
+    network_interface_id = aws_network_interface.private.id
+  }
+
+resource "aws_network_interface" "private" {
+    subnet_id = "subnet-0513abf3db97a734c"
+    private_ips = ["172.31.22.10"]
+    }
+  
+
+
+
 
 resource "aws_security_group" "Docker_SG" {
   ingress {
