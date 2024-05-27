06ad4abe8a981da370c8a8a87e3566ec7a8488b6
ritchiejames <automatealunkal@gmail.com>
2024-05-27 22:36:24 +0400

    make the deployment to  build/deployment/ansible server

--- main.tf
+++ main.tf
@@ -1,20 +1,16 @@
-
-#provider "aws" {
- # region = "us-east-1"
-#}
-
-##Jenkins server 
-resource "aws_instance" "Jenkins_server" {
+##Build server  with functionalities  Jenkins/Maven
+#AWS Instance Resource for Build server
+resource "aws_instance" "Build_server" {
   ami           = "ami-0bb84b8ffd87024d8"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.Jenkins_SG.id]
+  vpc_security_group_ids = [aws_security_group.Build_SG.id]
 
   tags = {
-    Name = "Jenkins_server"
+    Name = "Build_server"
   }
 }
-
-resource "aws_security_group" "Jenkins_SG" {
+# AWS Security Group Resource for Build server
+resource "aws_security_group" "Build_SG" {
   ingress {
     from_port        = 22
     to_port          = 22
@@ -28,37 +24,32 @@ resource "aws_security_group" "Jenkins_SG" {
     cidr_blocks      = ["0.0.0.0/0"]
   }
   tags = {
-    Name = "Jenkins_SG"
+    Name = "Build_SG"
   }
 }
-####Docker Host
-
-resource "aws_instance" "Docker_host" {
+####Deployment server with functionality of Docker
+#AWS Instance Resource for Deployment  server
+resource "aws_instance" "Deployment_server" {
   ami           = "ami-0bb84b8ffd87024d8"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.Jenkins_SG.id]
+  vpc_security_group_ids = [aws_security_group.Deployment_SG.id]
     tags = {
-    Name = "Docker_host"
+    Name = "Deployment_server"
   }
 }
 #Private ip interface
-
-resource "aws_network_interface_attachment" "private_attachment" {
-    instance_id = aws_instance.Docker_host.id
-    device_index = 1
-    network_interface_id = aws_network_interface.private.id
-  }
-
 resource "aws_network_interface" "private" {
     subnet_id = "subnet-0513abf3db97a734c"
     private_ips = ["172.31.22.10"]
     }
-  
-
-
-
-
-resource "aws_security_group" "Docker_SG" {
+    
+resource "aws_network_interface_attachment" "private_attachment" {
+    instance_id = aws_instance.Deployment_server.id
+    device_index = 1
+    network_interface_id = aws_network_interface.private.id
+  }
+# AWS Security Group Resource for Deployment  server
+resource "aws_security_group" "Deployment_SG" {
   ingress {
     from_port        = 22
     to_port          = 22
@@ -72,22 +63,20 @@ resource "aws_security_group" "Docker_SG" {
     cidr_blocks      = ["0.0.0.0/0"]
   }
   tags = {
-    Name = "Docker_SG"
+    Name = "Deployment_SG"
   }
 }
-
-##Ansible server
-
+##AWS Instance Resource for Ansible  server
 resource "aws_instance" "Ansible_server" {
   ami           = "ami-0bb84b8ffd87024d8"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.Jenkins_SG.id]
+  vpc_security_group_ids = [aws_security_group.Ansible_SG.id]
 
   tags = {
     Name = "Ansible_server"
   }
 }
-
+# AWS Security Group Resource for Ansible server
 resource "aws_security_group" "Ansible_SG" {
   ingress {
     from_port        = 22
